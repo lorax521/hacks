eb34d60d25cac9de4f80394dfcb114639bfff550
James Raines <jraines521@gmail.com>
2020-01-10 16:58:42 -0700

    Update react-app.bat

--- react-app.bat
+++ react-app.bat
@@ -5,3 +5,11 @@ npm install --save redux
 npm install --save @material-ui/core
 npm install --save @material-ui/icons
 npm install --save typeface-roboto
+mkdir src
+mkdir src/assets
+mkdir src/assets/images
+mkdir src/app
+mkdir src/app/actions
+mkdir src/app/components
+mkdir src/app/reducers
+mkdir src/app/services
